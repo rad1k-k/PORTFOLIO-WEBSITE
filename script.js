f9696af323f32e7ba9b8aad282fec37a15f090fa
Radka Krenova <radka.krenov@gmail.com>
2022-03-01 10:46:24 +0100

    JS for scroll to top button + JS for reveal elements on scroll

--- script.js
+++ script.js
@@ -22,3 +22,38 @@ navigationItems.forEach((navigationItem) => {
         navigation.classList.remove("active");
     });
 });
+
+
+
+//javascript for scroll to top button
+const scrollBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function() {
+    scrollBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+
+//javascript for scroll to top button on click to button
+scrollBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
+
+
+//javascript for reveal elemnts on scroll
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for(var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 50;
+
+        if(revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add("active")
+        }
+    }
+}
